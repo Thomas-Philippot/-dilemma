e72a6490754c0678a6c51981e275fbefde8a3614
thomas <tp456@hotmail.fr>
2020-10-03 00:07:58 +0200

    reverted SPA mode'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Disable server-side rendering (htt/ps:/go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - tu-prefere-marrons',
